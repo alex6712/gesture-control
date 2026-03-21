7cabb01ed2c868b075e68c1f5ccd25e95007d2f2
alex6712 <alexeivanyukov@yandex.ru>
2026-03-21 13:29:59 +0300

    Инициализация API Gateway

--- services/gateway/main.go
+++ services/gateway/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func wsHandler(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println("Ошибка апгрейда:", err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Клиент отключился:", err)
+			break
+		}
+
+		conn.WriteMessage(messageType, message)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, 世界!")
+	r := gin.Default()
+
+	r.GET("/ws", wsHandler)
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	r.Run(":8080")
 }
